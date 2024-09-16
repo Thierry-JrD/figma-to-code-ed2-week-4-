27eff75b6ed63866a4da14b2ce0d844e661a86ee
Hervé Zossou <hervezossou8@gmail.com>
2024-09-16 13:11:45 +0100

    Sidebar navigation

---
+++ Js/admin-dashboard/sidebar.js
@@ -0,0 +1,61 @@
+/* SIDEBAR NAVIGATION */
+
+const sidebar = document.getElementById('sidebar')
+
+const sidebarIcons = [
+    {
+        title: "home",
+        img: "assets/home.png",
+        href: "home.html"
+    },
+    {   
+        title: "calendar",
+        img: "assets/calendar.png",
+        href: "calendar.html"
+    },
+    {
+        title: "chats",
+        img: "assets/chats.png",
+        href: "chats.html"
+    },
+    {
+        title: "stats",
+        img: "assets/stats.png",
+        href: "stats.html"
+    },
+    {
+        title: "settings",
+        img: "assets/settings.png",
+        href: "settings.html"
+    },
+    {
+        title: "logout",
+        img: "assets/logout.png",
+        href: "logout.html"
+    }
+]
+
+const sidebarIconBuilder = (icon) => {
+    let content = 
+    `
+    <div class='sidebar-icon'>
+        <a href="${icon.href}"><img src="${icon.img}" alt="${icon.title}">
+    </div>
+    `
+
+    return content
+}
+
+const sidebarIconsRender = () => {
+    let sidebarContent = '';
+
+    sidebarIcons.map((icon) => {
+        sidebarContent += sidebarIconBuilder(icon);
+    });
+    
+    return sidebarContent
+}
+
+sidebar.innerHTML = `
+    <div class="sidebar-grid">${sidebarIconsRender()}</div>
+`
